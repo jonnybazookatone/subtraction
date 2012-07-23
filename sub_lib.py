549acea472ae559d0322d68a11c19c6e7745826e
Jonny Elliott <jonnyelliott@mpe.mpg.de>
2012-07-23 14:22:29 +0200

    develop version of sub_lib.py added to master

--- sub_lib.py
+++ sub_lib.py
@@ -42,13 +42,74 @@ def parseIni(filename):
 			sys.exit(0)
 			
 		equalline = line.split("=")
-		iniDict[dictname][equalline[0]] = equalline[1]
+		if equalline[1].replace(".","").isdigit():
+			iniDict[dictname][equalline[0]] = float(equalline[1])
+		else:
+			iniDict[dictname][equalline[0]] = equalline[1]
 		
-	print iniDict
+	return iniDict
 
+def parseRel(relfile):
+	
+	"""Parse the star file given for relative astronomy"""
+	relf = open(relfile, "r")
+	relfline = relf.readlines()
+	relf.close()
+	
+	# First line is GRB
+	grb = relfline[0].replace("\n","").split(" ")
+	GRBDict = {}
+	GRBDict["RA"] = float(grb[0])
+	GRBDict["DEC"] = float(grb[0])
+	
+	StarArray = []
+	# Other lines are magnitudes
+	for i in relfline[1:]:
+		  starline = i.replace("\n","").split(" ")
+		  StarDict = {}
+		  StarDict["RA"] = float(starline[0])
+		  StarDict["DEC"] = float(starline[1])
+		  StarDict["MAG_ABSOLUTE"] = float(starline[2])
+		  StarDict["MAG_ABSOLUTE_ERR"] = float(starline[3])
+		  
+		  StarArray.append(StarDict)
+		  
+	return GRBDict, StarArray
+
+def checkdist(obj):
+	try:
+		if obj["DISTANCE"]>2:
+			return -1
+		else:
+			return 0
+	except:
+		return -2
+
+def parseApp(infile):
+	# Load and parse
+	infile = open(infile, "r")
+	inline = infile.readlines()
+	infile.close()
+
+	OBArray = []
+	timeArray = []
+	timeErrArray = []
+	magArray = []
+	magErrArray = []
+	for i in inline:
+		lsplit = i.replace("\n", "").split(" ")
+
+		OBArray.append(lsplit[0])
+		timeArray.append(float(lsplit[1]))
+		timeErrArray.append(float(lsplit[2]))
+		magArray.append(float(lsplit[3]))
+		
+		magErrArray.append(float(lsplit[4]))
+	
+	return OBArray, timeArray, timeErrArray, magArray, magErrArray
 
 def main():
-	parseIni("test.ini")
+	print parseIni("test.ini")
 
 if __name__ == "__main__":
 	main()
